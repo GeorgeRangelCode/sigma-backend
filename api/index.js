870ed0474c2123a3985da541bae54c5a30a6369c
Jorge Rangel Jimenez <georgerangelcode@gmail.com>
2020-08-08 16:11:42 -0500

    Access-Control-Allow-Origin

--- api/index.js
+++ api/index.js
@@ -5,6 +5,27 @@ const config = require("../config.js");
 const contacts = require("./components/contacts/network");
 const app = express();
 
+app.use(function (req, res, next) {
+  res.setHeader(
+    "Access-Control-Allow-Origin",
+    "https://sigma-frontend.georgerangelcode.vercel.app"
+  );
+
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, OPTIONS, PUT, PATCH, DELETE"
+  );
+
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "X-Requested-With,content-type"
+  );
+
+  res.setHeader("Access-Control-Allow-Credentials", true);
+
+  next();
+});
+
 const swaggerDoc = require("./swagger.json");
 
 app.use(bodyParser.json());
